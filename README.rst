cf5644ff1f252c4e90ec88acafa5374bfa5d3647
ahmadiga <a.bazadough@sit-mena.com>
2016-03-24 18:24:49 +0200

    fixes

---
+++ README.rst
@@ -0,0 +1,1 @@
+curl -sL https://raw.githubusercontent.com/ahmadiga/sit-generator/master/start.sh | bash
--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 sudo apt-get install software-properties-common python python-dev python-pip git -y
 sudo pip install --upgrade pip
 sudo pip install virtualenv
+wget https://raw.githubusercontent.com/ahmadiga/sit-generator/master/start.py
 python start.py
-
