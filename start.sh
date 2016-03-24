d19d4a6dbcbed5fd390627ead4c319c547bf684e
ahmadiga <a.bazadough@sit-mena.com>
2016-03-24 19:44:11 +0200

    starter tool ready

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #sudo apt-get update
-sudo apt-get install software-properties-common python python-dev python-pip git -y
+sudo apt-get install software-properties-common python python-dev python-pip git npm -y
 sudo pip install --upgrade pip
 sudo pip install virtualenv
+sudo npm install npm -g
+sudo npm install -g bower
 wget https://raw.githubusercontent.com/ahmadiga/sit-generator/master/start.py
 python start.py
