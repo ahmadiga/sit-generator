3436a08d9f81b994c5a9dd437bfbd8f567675ed9
Ahmad Bazadough <a.bazadough@sit-mena.com>
2016-08-31 01:21:10 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 #sudo apt-get update
-sudo apt-get install software-properties-common python python-dev python-pip git npm -y
+sudo apt-get install software-properties-common python python-dev python-pip git npm ruby-dev rubygems -y
 sudo pip install --upgrade pip
 sudo pip install virtualenv
 sudo npm install npm -g
 sudo npm install -g bower
+sudo gem update --system
+sudo gem install compass
 wget https://raw.githubusercontent.com/ahmadiga/sit-generator/master/start.py
 python start.py
 rm start.py
